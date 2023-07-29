d7e0d916e5fc276360f5fd9cbe387f3952fff83a
evan0416 <cycloevan97@gmail.com>
2023-07-29 22:32:28 +0900

    code: Building a Blog Application

---
+++ Ch01/mysite/blog/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from .models import Post
+
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+# Create your views here.
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts' : posts})
+    
+    
+"""before using the get_object_or_404 shortcut"""
+"""
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+    
+    
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post' : post})
+    
+"""
+
+"""Using the get_object_or_404 shortcut"""
+def post_detail(request, id):
+    post = get_object_or_404(Post,
+                             id=id,
+                             status = Post.Status.PUBLISHED)
+    
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post' : post})
+
+    
+    
